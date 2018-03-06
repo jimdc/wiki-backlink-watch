27d35cd7ac5b49e15d2b68b7c9a001aa0122cec8
James Carroll <jamescarroll@protonmail.ch>
2018-03-06 12:22:03 -0500

    start WikiApiService interface

---
+++ src/main/kotlin/com/example/WikiApiService.kt
@@ -0,0 +1,86 @@
+package com.example
+
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WikiApiService {
+
+    /* https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=Trump */
+    @GET("api.php")
+    fun hitCountCheck(@Query("action") action: String = "query",
+                      @Query("format") format: String = "json",
+                      @Query("list") list: String = "search",
+                      @Query("srsearch") srsearch: String):
+            Observable<HitModel.Result>
+
+    /* https://en.wikipedia.org/w/api.php?action=query&format=json&list=backlinks
+       &bltitle=China&blnamespace=0&blfilterredir=nonredirects&bllimit=50 */
+    @GET("api.php")
+    fun backlinks(@Query("action") action: String = "query",
+                  @Query("format") format: String = "json",
+                  @Query("list") list: String = "backlinks",
+                  @Query("bltitle") bltitle: String,
+                  @Query("blnamespace") blnamespace: Int = 0, //Articles only
+                  @Query("blfilterredir") blfilterredir: String = "nonredirects",
+                  @Query("bllimit") bllimit: Int = 50):
+            Observable<BacklinkModel.Result>
+
+    companion object {
+        fun create(): WikiApiService {
+
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .baseUrl("https://en.wikipedia.org/w/")
+                    .build()
+
+            return retrofit.create(WikiApiService::class.java)
+        }
+    }
+
+    object HitModel {
+        data class Result(val query: Query)
+        data class Query(val searchinfo: SearchInfo)
+        data class SearchInfo(val totalhits: Int)
+    }
+
+/*
+{
+ batchcomplete: "",
+ continue: {
+  sroffset: 10,
+  continue: "-||"
+ },
+ query: {
+  searchinfo: {
+   totalhits: 16776
+ },
+ search: [...
+*/
+
+    object BacklinkModel {
+        data class Result(val query: Query)
+        data class Query(val backlinks: Backlink)
+        data class Backlink(val pageinfo: List<Pageinfo>)
+        data class Pageinfo(val pageid: Int, val ns: Int, val title: String)
+    }
+
+/*
+{"batchcomplete":"",
+ "continue": {
+     "blcontinue":"0|2175",
+     "continue":"-||"
+     },
+ "query": {
+    "backlinks":[
+       {"pageid":573,"ns":0,"title":"Alchemy"},
+       {"pageid":597,"ns":0,"title":"Austroasiatic languages"},...
+
+*/
+
+
+}
--- src/main/kotlin/com/example/Zhengmingapp.kt
+++ src/main/kotlin/com/example/Zhengmingapp.kt
@@ -1,9 +1,18 @@
 package com.example
 
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
 import com.example.activity.Prefs
+import com.example.WikiApiService
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.android.schedulers.AndroidSchedulers
 
+val wikiApiServe by lazy { WikiApiService.create() }
 val prefs: Prefs by lazy { Zhengmingapp.prefs!! }
+
+var disposable: Disposable? = null
 
 class Zhengmingapp : Application() {
     companion object {
@@ -14,4 +23,20 @@ class Zhengmingapp : Application() {
         prefs = Prefs(applicationContext)
         super.onCreate()
     }
+
+    private fun beginSearch(searchString: String) {
+        disposable = wikiApiServe.hitCountCheck("query", "json", "search", searchString)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result -> Log.v("Zhengmingapp", "${result.query.searchinfo.totalhits} result found") },
+                        { error -> Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show() }
+                )
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        disposable?.dispose()
+    }
+
 }
