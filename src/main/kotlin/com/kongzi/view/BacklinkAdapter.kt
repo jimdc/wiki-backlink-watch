22ea1fd02c4cf9c5cc13eda423a0cdbb63219228
James Carroll <jamescarroll@protonmail.ch>
2018-03-11 21:53:57 -0400

    open web edit on click

--- src/main/kotlin/com/kongzi/view/BacklinkAdapter.kt
+++ src/main/kotlin/com/kongzi/view/BacklinkAdapter.kt
@@ -1,10 +1,15 @@
 package com.kongzi.view
 
+import android.content.Intent
+import android.net.Uri
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import com.kongzi.model.WikiApiService.BacklinkModel.Backlink
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.kongzi.R
 
@@ -13,6 +18,18 @@ public class BacklinkAdapter(var blinks: List<Backlink>) : RecyclerView.Adapter<
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         var row1 = v.findViewById<TextView>(R.id.row1)
         var row2 = v.findViewById<TextView>(R.id.row2)
+        var bEdit = v.findViewById<ImageView>(R.id.imgEditLink)
+        init {
+            bEdit.setOnClickListener {
+                var url = "https://en.wikipedia.org/w/index.php?title=${row1.text}&action=edit"
+                var intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(url))
+                if (intent.resolveActivity(it.context.packageManager) != null) {
+                    it.context.startActivity(intent)
+                } else {
+                    Log.d("BacklinkAdapter", "No webbrowser to open edit link!")
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BacklinkAdapter.ViewHolder {
